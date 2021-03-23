1d43f20242c502078684251b8069651617e89ac4
gback207 <65297008+gback207@users.noreply.github.com>
2021-03-23 21:22:09 +0900

    Create pytorch_practive1.py

---
+++ pytorch_practive1.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+import torch
+
+#Tensor
+x1= torch.empty(5,3)
+print(x1)
+#초기화되지 않은 5x3 행렬을 생성합니다
+
+
+x2=torch.rand(5,3)
+print(x2)
+#무작위로 초기화된 행렬을 생성합니다
+
+x3= torch.zeros(5,3)
+print(x3)
+#0으로 채워진 행렬을 생성합니다
+
+x4= torch.tensor([[5.0,4.7,2.3],[1.2,3.5,6.1]])
+print(x4)
+#데이터로부터 tensor를 직접 생성합니다
+
+print(x1.size())
+#행렬의 크기를 구합니다
+
+
+#연산
+print(x1+x2)
+print(torch.add(x1,x2))
+print(x1.add(x2))
+#덧셈문법
+
+

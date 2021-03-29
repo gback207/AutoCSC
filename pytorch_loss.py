6892bef131aaf6f18d5177bf1309f1b20ac07153
gback207 <65297008+gback207@users.noreply.github.com>
2021-03-29 21:29:47 +0900

    Update pytorch_loss.py

--- pytorch_loss.py
+++ pytorch_loss.py
@@ -2,8 +2,3 @@ import torch
 import torch.nn as nn
 import numpy as np
 
-cross_entropy_loss=nn.CrossEntropyLoss()
-output=torch.Tensor([[0.5,0.2,0.3]])
-target=torch.Tensor([1,0,0])
-loss=cross_entropy_loss(output,target)
-print(loss)

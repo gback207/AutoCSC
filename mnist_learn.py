bd2fb444b67db8fa456bca1c1e01aa0fe4001a5a
gback207 <65297008+gback207@users.noreply.github.com>
2021-04-01 21:50:57 +0900

    Update mnist_learn.py

--- mnist_learn.py
+++ mnist_learn.py
@@ -5,36 +5,33 @@ import torchvision.utils as utils
 import torchvision.datasets as dsets
 import torchvision.transforms as transforms
 from matplotlib import pyplot as plt
-from pytorch_BasicNeuralNetwork import *
+from pytorch_Basicmodel import *
+import torch.optim as optim
 
 train_data = dsets.MNIST(root='data/', train=True, transform=transforms.ToTensor(), download=True)
 test_data  = dsets.MNIST(root='data/', train=False, transform=transforms.ToTensor(), download=True)
 
+Model=model(784,50,10)
+optimizer=optim.SGD(Model.parameters(),lr=0.01)
+loss_fn=nn.CrossEntropyLoss()
 
+for i in range(1000):
+    optimizer.zero_grad()
+    input=train_data[i][0].view(784)
+    print(input)
+    label=train_data[i][1]
+    print(label)
+    pre_target=torch.zeros(10)
+    pre_target[label]=1
+    target=torch.tensor(pre_target, dtype=torch.long)
+    print(target)
+    output=Model.forward(input)
+    print(output)
+    loss=loss_fn(output,target)
+    if i%100==0:
+        print(loss)
+    optimizer.step()
 
-TLN=Two_Layer_Net(784,50,10,1)
-
-for i in range(10000):
-    x=train_data[i][0].view(1,784)
-    print(x.shape)
-    t=torch.zeros(1,10)
-    t[0][train_data[i][1]]=1
-    print(t)
-    W1 = TLN.params['W1']
-    print(W1)
-    W2 = TLN.params['W2']
-    print(W2)
-    L=TLN.loss(x,t)
-    L.backward()
-
-    print(W1.retain_grad())
-
-    loss_list=[]
-    loss_list.append(L)
-
-
-
-print(loss_list)
 
 
 

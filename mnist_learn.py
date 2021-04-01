bcdc005b03c22367b59bc9150224c9a5964857cd
gback207 <65297008+gback207@users.noreply.github.com>
2021-04-02 08:21:14 +0900

    Update mnist_learn.py

--- mnist_learn.py
+++ mnist_learn.py
@@ -7,27 +7,30 @@ import torchvision.transforms as transforms
 from matplotlib import pyplot as plt
 from pytorch_Basicmodel import *
 import torch.optim as optim
+from torch.autograd import Variable
 
 train_data = dsets.MNIST(root='data/', train=True, transform=transforms.ToTensor(), download=True)
 test_data  = dsets.MNIST(root='data/', train=False, transform=transforms.ToTensor(), download=True)
 
 Model=model(784,50,10)
+print(Model.parameters())
 optimizer=optim.SGD(Model.parameters(),lr=0.01)
 loss_fn=nn.CrossEntropyLoss()
 
-for i in range(1000):
+for i in range(10000):
     optimizer.zero_grad()
-    input=train_data[i][0].view(784)
-    print(input)
+    input=Variable(train_data[i][0].view(784), requires_grad=True)
+    input=input.view(1,-1)
+    #print(input)
     label=train_data[i][1]
-    print(label)
-    pre_target=torch.zeros(10)
-    pre_target[label]=1
-    target=torch.tensor(pre_target, dtype=torch.long)
-    print(target)
-    output=Model.forward(input)
-    print(output)
+    #print(label)
+    target=torch.tensor([label], dtype=torch.long)
+    #pre_target[label]=1
+    #print(target)
+    output=Variable(Model.forward(input), requires_grad=True)
+    #print(output)
     loss=loss_fn(output,target)
+    loss.backward()
     if i%100==0:
         print(loss)
     optimizer.step()

aaabd9a8f34697df6a298362f3c7795c80738064
gback207 <65297008+gback207@users.noreply.github.com>
2021-04-28 09:21:40 +0900

    Update main_projrct.py

--- main_projrct.py
+++ main_projrct.py
@@ -15,10 +15,10 @@ def unpickle(file):
         dict = pickle.load(fo, encoding='bytes')
     return dict
 
-f=unpickle(r'C:\Users\brian\Downloads\cifar-10-python (1).tar\cifar-10-batches-py\data_batch_1')
+f1=unpickle(r'C:\Users\brian\Downloads\cifar-10-python (1).tar\cifar-10-batches-py\data_batch_1')
 
 
-pre_data= torch.from_numpy(f[b'data'])
+pre_data= torch.from_numpy(f1[b'data'])
 data=torch.zeros(10000,3,32,32)
 for i in range(len(pre_data)):
     r=pre_data[i][0:1024]
@@ -34,17 +34,19 @@ for i in range(len(pre_data)):
 
 print(data[0].shape)
 
-labels= f[b'labels']
+pre_labels= f1[b'labels']
 
 
-train_data=[]
-
+labels=torch.zeros(100,100, dtype=torch.long)
 batch_data= torch.zeros(100,100,3,32,32)
 for i in range(100):
     for j in range(100):
-        batch_data[i][j]= data[j]
+        batch_data[i][j]= data[i*100+j]
+        labels[i][j]=pre_labels[i*100+j]
 
-print(batch_data[0].shape)
+
+print(batch_data[26].shape)
+print(labels[36].shape)
 
 
 
@@ -71,14 +73,55 @@ cnn = CNN()
 criterion = torch.nn.CrossEntropyLoss()
 optimizer = optim.SGD(cnn.parameters(), lr=0.01)
 
+
+
 cnn.train()
-for i in range(len(batch_data[i])):
+for i in range(100):
     optimizer.zero_grad()
     output=cnn.forward(batch_data[i])
-    target=batch_data[i][1]
+    target=labels[i]
     loss = criterion(output, target)
     loss.backward()
     optimizer.step()
     print(loss)
 
 
+
+f2=unpickle(r'C:\Users\brian\Downloads\cifar-10-python (1).tar\cifar-10-batches-py\test_batch')
+
+pre_test_data=torch.from_numpy(f2[b'data'])
+pre_test_labels=f2[b'labels']
+
+print(pre_test_data.shape)
+print(len(pre_test_labels))
+
+test_data=torch.zeros(10000,3,32,32)
+for i in range(len(pre_data)):
+    r=pre_test_data[i][0:1024]
+    g=pre_test_data[i][1024:2048]
+    b=pre_test_data[i][2048:3072]
+    r=r.view(32,32)
+    g=g.view(32,32)
+    b=b.view(32,32)
+    test_data[i][0]=r
+    test_data[i][1]=g
+    test_data[i][2]=b
+
+test_labels=torch.zeros(100,100, dtype=torch.long)
+test_batch_data= torch.zeros(100,100,3,32,32)
+for i in range(100):
+    for j in range(100):
+        test_batch_data[i][j]=test_data[i*100+j]
+        test_labels[i][j]=pre_test_labels[i*100+j]
+
+
+counter=0
+for i in range(100):
+    optimizer.zero_grad()
+    output = cnn.forward(test_batch_data[i])
+    target = test_labels[i]
+    for j in range(100):
+        if torch.argmax(output[j])==target[j]:
+            counter+=1
+
+print(counter/10000)

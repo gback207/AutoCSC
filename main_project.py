aaed10cf32995464a958d93225d0ecdfde63f9a1
gback207 <65297008+gback207@users.noreply.github.com>
2021-05-27 20:58:57 +0900

    Update main_project.py

--- main_project.py
+++ main_project.py
@@ -14,92 +14,21 @@ def unpickle(file):
         dict = pickle.load(fo, encoding='bytes')
     return dict
 
-f1=unpickle(r'C:\Users\brian\Downloads\cifar-10-python (1).tar\cifar-10-batches-py\data_batch_1')
-f2=unpickle(r"C:\Users\brian\Downloads\cifar-10-python (1).tar\cifar-10-batches-py\data_batch_2")
-f3=unpickle(r"C:\Users\brian\Downloads\cifar-10-python (1).tar\cifar-10-batches-py\data_batch_3")
-f4=unpickle(r'C:\Users\brian\Downloads\cifar-10-python (1).tar\cifar-10-batches-py\data_batch_4')
-f5=unpickle(r'C:\Users\brian\Downloads\cifar-10-python (1).tar\cifar-10-batches-py\data_batch_5')
+f1=unpickle(r'C:\Users\brian\OneDrive\바탕 화면\cifar-10-python (1).tar\cifar-10-batches-py\data_batch_1')
+f2=unpickle(r"C:\Users\brian\OneDrive\바탕 화면\cifar-10-python (1).tar\cifar-10-batches-py\data_batch_2")
+f3=unpickle(r"C:\Users\brian\OneDrive\바탕 화면\cifar-10-python (1).tar\cifar-10-batches-py\data_batch_3")
+f4=unpickle(r'C:\Users\brian\OneDrive\바탕 화면\cifar-10-python (1).tar\cifar-10-batches-py\data_batch_4')
+f5=unpickle(r'C:\Users\brian\OneDrive\바탕 화면\cifar-10-python (1).tar\cifar-10-batches-py\data_batch_5')
 
 data_dict={'b1':f1, 'b2':f2, 'b3':f3, 'b4':f4, 'b5':f5}
 
-
-class CNN(nn.Module):
-    def __init__(self):
-        super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=20, kernel_size=5, stride=1)
-        self.conv2 = nn.Conv2d(in_channels=20, out_channels=50, kernel_size=5, stride=1)
-        self.fc1 = nn.Linear(5 * 5 * 50, 500)
-        self.fc2 = nn.Linear(500, 10)
-
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, kernel_size=2, stride=2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, kernel_size=2, stride=2)
-
-        x = x.view(-1, 5 * 5 * 50)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
-
-
-cnn = CNN()
-criterion = torch.nn.CrossEntropyLoss()
-optimizer = optim.Adagrad(cnn.parameters(), lr=0.001)
-
-
-
-for epoch in range(5):
-    for key in data_dict.keys():
-        pre_data= torch.from_numpy(data_dict[key][b'data'])
-        data=torch.zeros(10000,3,32,32)
-        for i in range(len(pre_data)):
-            r=pre_data[i][0:1024]
-            g=pre_data[i][1024:2048]
-            b=pre_data[i][2048:3072]
-            r=r.view(32,32)
-            g=g.view(32,32)
-            b=b.view(32,32)
-            data[i][0]=r
-            data[i][1]=g
-            data[i][2]=b
-
-
-        pre_labels=data_dict[key][b'labels']
-
-
-        labels=torch.zeros(100,100, dtype=torch.long)
-        batch_data= torch.zeros(100,100,3,32,32)
-        for i in range(100):
-            for j in range(100):
-                batch_data[i][j]= data[i*100+j]
-                labels[i][j]=pre_labels[i*100+j]
-
-
-
-
-        cnn.train()
-        for i in range(100):
-            optimizer.zero_grad()
-            output=cnn(batch_data[i])
-            target=labels[i]
-            loss = criterion(output, target)
-            loss.backward()
-            optimizer.step()
-        print(loss)
-
-
-
-ft=unpickle(r'C:\Users\brian\Downloads\cifar-10-python (1).tar\cifar-10-batches-py\test_batch')
+ft=unpickle(r'C:\Users\brian\OneDrive\바탕 화면\cifar-10-python (1).tar\cifar-10-batches-py\test_batch')
 
 pre_test_data=torch.from_numpy(ft[b'data'])
 pre_test_labels=ft[b'labels']
 
-print(pre_test_data.shape)
-print(len(pre_test_labels))
-
 test_data=torch.zeros(10000,3,32,32)
-for i in range(len(pre_data)):
+for i in range(len(pre_test_data)):
     r=pre_test_data[i][0:1024]
     g=pre_test_data[i][1024:2048]
     b=pre_test_data[i][2048:3072]
@@ -109,7 +38,6 @@ for i in range(len(pre_data)):
     test_data[i][0]=r
     test_data[i][1]=g
     test_data[i][2]=b
-
 test_labels=torch.zeros(100,100, dtype=torch.long)
 test_batch_data= torch.zeros(100,100,3,32,32)
 for i in range(100):
@@ -118,14 +46,90 @@ for i in range(100):
         test_labels[i][j]=pre_test_labels[i*100+j]
 
 
-counter=0
-cnn.eval()
-for i in range(100):
-    optimizer.zero_grad()
-    output = cnn(test_batch_data[i])
-    target = test_labels[i]
-    for j in range(100):
-        if torch.argmax(output[j])==target[j]:
-            counter+=1
+batchdict={}
+for key in data_dict.keys():
+    pre_data= torch.from_numpy(data_dict[key][b'data'])
+    data=torch.zeros(10000,3,32,32)
+    for i in range(len(pre_data)):
+        r=pre_data[i][0:1024]
+        g=pre_data[i][1024:2048]
+        b=pre_data[i][2048:3072]
+        r=r.view(32,32)
+        g=g.view(32,32)
+        b=b.view(32,32)
+        data[i][0]=r
+        data[i][1]=g
+        data[i][2]=b
 
-print(counter/10000)
+
+    pre_labels=data_dict[key][b'labels']
+
+
+    labels=torch.zeros(100,100, dtype=torch.long)
+    batch_data= torch.zeros(100,100,3,32,32)
+    for i in range(100):
+        for j in range(100):
+            batch_data[i][j]= data[i*100+j]
+            labels[i][j]=pre_labels[i*100+j]
+    batchdict[key]={'batch_data':batch_data,'labels':labels}
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=20, kernel_size=5, stride=1)
+        self.conv2 = nn.Conv2d(in_channels=20, out_channels=50, kernel_size=5, stride=1)
+        self.fc1 = nn.Linear(5 * 5 * 50, 500)
+        self.fc2 = nn.Linear(500, 10)
+        self.bn1 = nn.BatchNorm2d(20)
+        self.bn2 = nn.BatchNorm2d(50)
+        self.bn3= nn.BatchNorm1d(500)
+        self.bn4=nn.BatchNorm1d(10)
+
+
+
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.max_pool2d(x, kernel_size=2, stride=2)
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.max_pool2d(x, kernel_size=2, stride=2)
+
+        x = x.view(-1, 5 * 5 * 50)
+        x = F.relu(self.bn3(self.fc1(x)))
+        x = F.relu(self.bn4(self.fc2(x)))
+        return F.log_softmax(x, dim=1)
+
+
+cnn = CNN()
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = optim.Adagrad(cnn.parameters(), lr=0.001)
+
+
+loss_list=[]
+accuracy_list=[]
+
+for epoch in range(300):
+    for key in data_dict.keys():
+        cnn.train()
+        for i in range(100):
+            optimizer.zero_grad()
+            output=cnn(batchdict[key]['batch_data'][i])
+            target=batchdict[key]['labels'][i]
+            loss = criterion(output, target)
+            loss.backward()
+            optimizer.step()
+        print(loss.item())
+        loss_list.append(loss.item())
+
+        counter=0
+        cnn.eval()
+        for i in range(100):
+            optimizer.zero_grad()
+            output = cnn(test_batch_data[i])
+            target = test_labels[i]
+            for j in range(100):
+                if torch.argmax(output[j])==target[j]:
+                    counter+=1
+
+        print(counter/10000)
+        accuracy_list.append(counter/10000)
